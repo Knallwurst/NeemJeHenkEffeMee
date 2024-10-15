d99c296c8394a2004060ef8021484471968442cf
Knallwurst <mvlinbox@gmail.com>
2024-10-16 00:09:00 +0200

    root files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Specify the port here
+    strictPort: true, // This ensures that Vite will fail if port 5173 is unavailable
+  },
+  build: {
+    sourcemap: true,  // Enable source maps for better debugging
+  }
 })
